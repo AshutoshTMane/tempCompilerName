06c04c1a15bd542bfefc7f991afdba1a43a6dc98
Ashutosh Mane <53065203+AshutoshTMane@users.noreply.github.com>
2024-11-15 16:35:33 -0800

    Fixed up the tokenizer, I believe it works now

--- compiler-files/lexer.py
+++ compiler-files/lexer.py
@@ -1,9 +1,8 @@
 import re
 
 class LexicalAnalyzer:
-    
-    def tokenize():
-        token_rules = [
+    def __init__(self):
+        self.token_rules = [
             # Reserved keywords
             ('MAIN', r'main'),
             ('IF', r'if'), 
@@ -35,16 +34,31 @@ class LexicalAnalyzer:
             ('RPAREN', r'\)'),
             ('LBRACE', r'\{'),
             ('RBRACE', r'\}'),
-            
+                
             # Identifiers and numbers
             ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*'), # since identifiers can be anything, we have rules saying the identifier can be a-z or A-Z with underscores. Note that the first character can not be a digit, which is why we have two brackets, the second one using * to denote that we can have 'zero or more' occurrences
             ('NUMBER', r'\d+'),  # \d in regex means "string contains digits", so this means one or more occurrences of a digit
-            
+                
             # Comments (skip comments)
             ('COMMENT', r'\/\/[^\n]*'), 
-            
+                
             # Whitespace (skip these characters)
             ('WHITESPACE', r'\s+'),
         ]
+
+        self.token_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in self.token_rules)
     
+    def tokenize(self, code):
+    
+        tokens = []
+
+        # The regex 'finditer' function essentially finds a substring from our token_list and then turns them as an "iterator". This means we get a special object that gives us the group name and matched test in a "one at a time" fashion.
+        for token in re.finditer(self.token_regex, code): 
+            token_type = token.lastgroup  # The name of group that matched 
+            token_value = token.group()  # The actual text matched
+            if token_type not in {'WHITESPACE', 'COMMENT'}:  # Skip ignored tokens
+                tokens.append((token_type, token_value))
+        return tokens
+        
+        
 
--- compiler-files/main.py
+++ compiler-files/main.py
@@ -1,7 +1,21 @@
+from lexer import LexicalAnalyzer 
 
+if __name__ == "__main__":
+    code = """
+    main {
+        print(10 + 20);
+        // This is a comment
+        if (x > 5) {
+            x = x - 1;
+        }
+    }
+    """
 
-def main():
-    print()
+    # Instantiate the lexical analyzer
+    analyzer = LexicalAnalyzer()
+    # Tokenize the provided code
+    tokens = analyzer.tokenize(code)
 
-if __name__ == '__main__':
-    main()
+    # Print the list of tokens
+    for token in tokens:
+        print(token)
