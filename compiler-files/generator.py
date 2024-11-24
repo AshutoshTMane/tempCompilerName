af6a7fa55251a02e85773ddaf5ff0dc8d7332584
Ashutosh Mane <53065203+AshutoshTMane@users.noreply.github.com>
2024-11-23 17:37:52 -0800

    Generate function function

--- compiler-files/generator.py
+++ compiler-files/generator.py
@@ -1,6 +1,8 @@
 from syntax import *
 
 class CodeGenerator:
+
+
     def generate_assembly(self, ast):
         assembly_code = []
         
@@ -8,3 +10,7 @@ class CodeGenerator:
             code += self.generate_function(function)
 
         return assembly_code
+    
+    def generate_function(self, function):
+        for statement in function.body:
+            code += self.generate_statement(statement)
